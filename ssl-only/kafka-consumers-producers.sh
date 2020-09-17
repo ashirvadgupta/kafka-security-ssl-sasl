6c5fe606eee154ee849fb6e740870bd71936fa47
Ashirvad Gupta <ashirvadrocks@gmail.com>
2020-09-17 14:56:41 +0530

    Changes to Certs for SSL:

--- ssl-only/kafka-consumers-producers.sh
+++ ssl-only/kafka-consumers-producers.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --network kafka-cluster-network confluentinc/cp-kafka:5.0.1 \
-kafka-topics --zookeeper zookeeper-1:22181 --create --topic test --partitions 10 --replication-factor 3
+docker run -it --rm --network kafka-cluster-network confluentinc/cp-kafka:5.0.1 kafka-topics --zookeeper zookeeper-1:22181 --create --topic test --partitions 10 --replication-factor 3
 
+cd ssl-only
 #Console producer with SSL files mapped in the container
-docker run -it --rm -v ${KAFKA_SSL_SECRETS_DIR}/producer:/etc/kafka/secrets \
--v ${KAFKA_SSL_SECRETS_DIR}/host.producer.ssl.config:/etc/kafka/secrets/host.producer.ssl.config  \
---network kafka-cluster-network confluentinc/cp-kafka:5.0.1  kafka-console-producer \
---broker-list kafka-broker-1:19093,kafka-broker-2:29093,kafka-broker-3:39093 --topic test \
---producer.config /etc/kafka/secrets/host.producer.ssl.config
+export KAFKA_SSL_SECRETS_DIR=$PWD/../secrets && docker run -it --rm -v ${KAFKA_SSL_SECRETS_DIR}/producer:/etc/kafka/secrets -v ${KAFKA_SSL_SECRETS_DIR}/host.producer.ssl.config:/etc/kafka/secrets/host.producer.ssl.config --network kafka-cluster-network confluentinc/cp-kafka:5.0.1  kafka-console-producer --broker-list kafka-broker-1:19093,kafka-broker-2:29093,kafka-broker-3:39093 --topic test --producer.config /etc/kafka/secrets/host.producer.ssl.config
 
 
 #Console consumer with SSL files mapped in the container
-docker run -it --rm -v ${KAFKA_SSL_SECRETS_DIR}/consumer:/etc/kafka/secrets \
--v ${KAFKA_SSL_SECRETS_DIR}/host.consumer.ssl.config:/etc/kafka/secrets/host.consumer.ssl.config \
---network kafka-cluster-network confluentinc/cp-kafka:5.0.1 \
-kafka-console-consumer --bootstrap-server kafka-broker-1:19093,kafka-broker-2:29093,kafka-broker-3:39093 --topic test --from-beginning \
---consumer.config /etc/kafka/secrets/host.consumer.ssl.config
+export KAFKA_SSL_SECRETS_DIR=$PWD/../secrets && docker run -it --rm -v ${KAFKA_SSL_SECRETS_DIR}/consumer:/etc/kafka/secrets -v ${KAFKA_SSL_SECRETS_DIR}/host.consumer.ssl.config:/etc/kafka/secrets/host.consumer.ssl.config --network kafka-cluster-network confluentinc/cp-kafka:5.0.1 kafka-console-consumer --bootstrap-server kafka-broker-1:19093,kafka-broker-2:29093,kafka-broker-3:39093 --topic test --from-beginning --consumer.config /etc/kafka/secrets/host.consumer.ssl.config
+
+
+#Kafka Console
+docker run -itd --name kafka-console --network kafka-cluster-network confluentinc/cp-kafka:5.0.1 /bin/bash
+
+docker exec -it kafka-console /bin/bash
+
+
+#List Topics
+kafka-topics --zookeeper zookeeper-1:22181 --list
+
+#Describe Topic Test
+kafka-topics --zookeeper zookeeper-1:22181 --describe --topic test
